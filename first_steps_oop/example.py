4c6e3359b9fc56a8f0e3b2dbe583e62beedac9fb
MladenAngelov-dev <123299807+MladenAngelov-dev@users.noreply.github.com>
2023-10-26 18:28:09 +0300

    asd

---
+++ first_steps_oop/example.py
@@ -0,0 +1,12 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+        def has_birthday(self):
+            self.age += 1
+
+
+ivan = Person("Ivan", 20)
+print(ivan)
