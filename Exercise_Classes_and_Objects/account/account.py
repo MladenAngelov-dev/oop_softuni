5422fe0e203967dff6b60321858176147800ff09
MladenAngelov-dev <123299807+MladenAngelov-dev@users.noreply.github.com>
2023-10-31 09:58:43 +0200

    asd

---
+++ Exercise_Classes_and_Objects/account/account.py
@@ -0,0 +1,5 @@
+
+class Account:
+
+    def __init__(self, id: int, name: str, balance = 0):
+        pass
---
+++ first_steps_oop/pokemon_battle/project/pokemon.py
@@ -0,0 +1,5 @@
+
+class Pokemon:
+    def __init__(self, name , health):
+        self.name = name
+        self.health = health
