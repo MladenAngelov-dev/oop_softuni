7c1f7e9b2f274596f5ea328eb6cbcdf4f99461a0
Mladen Angelov <123299807+MladenAngelov-dev@users.noreply.github.com>
2023-11-02 19:14:23 +0200

    Add files via upload

--- Exercise_Classes_and_Objects/library/project/library.py
+++ Exercise_Classes_and_Objects/library/project/library.py
@@ -1,11 +1,12 @@
 from typing import List, Dict
 from project.user import User
 
+
 class Library:
 
     def __init__(self):
         self.user_records: List[User] = []
-        self.books_available = {}
+        self.books_available: Dict[str, List[str]] = {}
         self.rented_books = {}
 
     def get_book(self, author: str, book_name: str, days_to_return: int, user: User):
@@ -21,16 +22,16 @@ class Library:
                 if user.username not in self.rented_books:
                     self.rented_books[user.username] = {}
                 self.rented_books[user.username][book_name] = days_to_return
-
                 return f"{book_name} successfully rented for the next {days_to_return} days!"
-        for book in self.rented_books.values():
-            if book_name in book:
-                days = book[book_name]
-                return f'The book {book_name} is already rented and will be available in {days} days!'
+
+        for b in self.rented_books.values():
+            if book_name in b:
+                days = b[book_name]
+                return f'The book "{book_name}" is already rented and will be available in {days} days!'
 
     def return_book(self, author: str, book_name: str, user: User):
         if book_name not in user.books:
-            return f"{user. username} doesn't have this book in his/her records!"
+            return f"{user.username} doesn't have this book in his/her records!"
         user.books.remove(book_name)
         self.books_available[author].append(book_name)
         self.rented_books[user.username].pop(book_name)
